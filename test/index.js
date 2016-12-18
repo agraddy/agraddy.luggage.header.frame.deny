c803658a047bd1a9ca0150e7b4f618a80ff73c6f
Anthony Graddy <anthony@locationq.com>
2016-12-18 06:01:58 +0000

    Working

--- test/index.js
+++ test/index.js
@@ -1,7 +1,17 @@
 var tap = require('agraddy.test.tap')(__filename);
+var response = require('agraddy.test.res');
+var pack = require('../package.json');
 
 var mod = require('../');
 
-tap.assert.equal(mod.main('result'), 'result', 'Should be equal.');
+var req = {};
+var res = response();
+var lug = {};
+mod.luggage(req, res, lug, function() {
+	tap.assert.deepEqual(res._headers[0], {"X-Frame-Options": "DENY"}, 'Should set the header.');
+});
 
+// TODO: package.json needs to have a luggage item
+
+tap.assert.equal(pack.luggage, true, 'The luggage item needs to be set in the package.json file.');
 
